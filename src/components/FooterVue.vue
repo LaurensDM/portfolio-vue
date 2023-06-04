3a39da1834fb45acb0e33bd1480ad5be2344c0fd
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-06-04 16:57:42 +0200

    Fixing images

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -4,16 +4,16 @@
     class="bg-black text-center text-white flex flex-col p-5"
   >
   <div class="container mb-10">  
-  <form id="contact" data-netlify="true" method="POST" class="mx-auto flex flex-col max-w-lg px-16 py-10 gap-3 rounded-xl bg-darkShade">
+  <form id="contact" name="contact" netlify method="POST" class="mx-auto flex flex-col max-w-lg px-16 py-10 gap-3 rounded-xl bg-darkShade">
     <h4>Contact me!</h4>
     <fieldset>
-      <input name="subject" placeholder="Your name" type="text" tabindex="1" required autofocus class="w-full form-input rounded text-black">
+      <input name="name" placeholder="Your name" type="text" tabindex="1" required autofocus class="w-full form-input rounded text-black">
     </fieldset>
     <fieldset>
       <input placeholder="Your E-mail" name="email" type="email" tabindex="3" required class="w-full form-input rounded text-black">
     </fieldset>
     <fieldset>
-      <textarea placeholder="Type your message here...." name="body" tabindex="4" required class="w-full form-textarea rounded text-black"></textarea>
+      <textarea placeholder="Type your message here...." name="message" tabindex="4" required class="w-full form-textarea rounded text-black"></textarea>
     </fieldset>
     <fieldset>
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending" class="bg-regular px-3 py-2 rounded">Submit</button>
