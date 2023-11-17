bb9b3a70ffc099ead2854311cfeefa1aa0d1840f
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-11-17 17:33:31 +0100

    small styling fix

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,11 +1,11 @@
 <template>
     <v-card
-    class="mx-auto rounded hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 bg-slate "
+    class="mx-auto b rounded hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 bg-slate border-black"
     max-width="600"
     min-height="500"
   >
     <v-img
-      class="align-end text-regular rounded bg-repeat-round"
+      class="align-end text-regular rounded bg-dark bg-opacity-70"
       height="300"
       :src="`${project.img}`"
       cover
@@ -17,8 +17,8 @@
       <a :href="`${project.url}`" class="text-regular hover:underline" target="_blank">Link to project</a>
     </v-card-subtitle>
 
-    <v-card-text>
-      <div>{{ project.description }}</div>
+    <v-card-text class="v-text">
+      <div >{{ project.description }}</div>
     </v-card-text>
     <v-card-subtitle>
       <span class="text-darkShade font-bold uppercase">Status: {{ project.status }}</span>
@@ -42,3 +42,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+.v-text {
+  min-height: 72px;
+}
+</style>
